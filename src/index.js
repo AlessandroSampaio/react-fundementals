08d13ed15e8b21a684d93c4a254db57975d23e46
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-10-23 16:16:46 -0300

    Criando o primeiro componente

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./App";
 
 ReactDom.render(
-    <h1 id="title">
-        <span>Olá Mundo!!!</span>
-    </h1>,
+    <App />,
     document.getElementById('root'),
 );
