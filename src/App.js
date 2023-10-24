da0351e2e69cdf296da319d765f0bfc645a9d3cf
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-10-24 10:22:06 -0300

    PropTypes

--- src/App.js
+++ src/App.js
@@ -2,32 +2,41 @@ import React from "react";
 import Post from "./Post";
 import Header from "./Header";
 
-const title = 'JStack`s Blog';
+const title = "JStack`s Blog";
 
 function App() {
-    return (
-        <>
-            <Header  title={title}>
-                <h2>Posts da Semana</h2>
-            </Header>
-            <hr />
+  return (
+    <>
+      <Header >
+        <h2>Posts da Semana</h2>
+      </Header>
+      <hr />
 
-            <Post
-                title="Titulo da Noticia 01"
-                subtitle="Subtitulo da Noticia 01"
-            />
+      <Post
+        likes={20}
+        post={{
+          title: "Titulo da Noticia 01",
+          subtitle: "Subtitulo da Noticia 01",
+        }}
+      />
 
-            <Post
-                title="Titulo da Noticia 02"
-                subtitle="Subtitulo da Noticia 02"
-            />
+      <Post
+        likes={50}
+        post={{
+          title: "Titulo da Noticia 02",
+          subtitle: "Subtitulo da Noticia 02",
+        }}
+      />
 
-            <Post
-                title="Titulo da Noticia 03"
-                subtitle="Subtitulo da Noticia 03"
-            />
-        </>
-    );
+      <Post
+        likes={30}
+        post={{
+          title: "Titulo da Noticia 03",
+          subtitle: "Subtitulo da Noticia 03",
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
