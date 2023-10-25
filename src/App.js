f74af00c99a5c7cedcdbf997c2e2bb9099252409
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-10-25 08:21:43 -0300

    Trabalhando com states

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import Header from "./Header";
 
-const posts = [
-  {
-    title: 'Title #01', subtitle: 'Subtitle #01', likes: 20
-  },
-
-  {
-    title: 'Title #02', subtitle: 'Subtitle #02', likes: 40
-  },
-
-  {
-    title: 'Title #03', subtitle: 'Subtitle #03', likes: 120
-  },
-];
-
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      id: Math.random(), title: 'Title #01', subtitle: 'Subtitle #01', likes: 20
+    },
+  
+    {
+      id: Math.random(), title: 'Title #02', subtitle: 'Subtitle #02', likes: 40
+    },
+  
+    {
+      id: Math.random(), title: 'Title #03', subtitle: 'Subtitle #03', likes: 120
+    },
+  ]);
+
+  function handleRefresh() {
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random,
+        title: `Title #0${posts.length + 1}`,
+        subtitle: `Subtitle #0${posts.length + 1}`,
+        likes: 50
+      }
+    ]);
+  };
+
+  
   return (
     <>
       <Header >
         <h2>Posts da Semana</h2>
+        <button onClick={handleRefresh}>Atualizar</button>
       </Header>
       <hr />
 
