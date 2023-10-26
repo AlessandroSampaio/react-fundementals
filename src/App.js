b290ce71cd47ff44643750bbd5cece3c4c9245fa
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-10-26 11:55:48 -0300

    Utilizando a ContextApi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import Post from "./Post";
 import Header from "./Header";
+import Button from "./Button";
+import { ThemeProvider } from "./ThemeContext";
+
 
 function App() {
 
   const [posts, setPosts] = useState([
     {
-      id: Math.random(), title: 'Title #01', subtitle: 'Subtitle #01', likes: 20, read: true,
+      id: Math.random(),
+      title: "Title #01",
+      subtitle: "Subtitle #01",
+      likes: 20,
+      read: true,
     },
-  
+
     {
-      id: Math.random(), title: 'Title #02', subtitle: 'Subtitle #02', likes: 40, read: true,
+      id: Math.random(),
+      title: "Title #02",
+      subtitle: "Subtitle #02",
+      likes: 40,
+      read: true,
     },
-  
+
     {
-      id: Math.random(), title: 'Title #03', subtitle: 'Subtitle #03', likes: 120, read: false,
+      id: Math.random(),
+      title: "Title #03",
+      subtitle: "Subtitle #03",
+      likes: 120,
+      read: false,
     },
   ]);
 
+ 
   function handleRefresh() {
     setPosts((prevState) => [
       ...prevState,
@@ -25,40 +41,37 @@ function App() {
         id: Math.random,
         title: `Title #0${posts.length + 1}`,
         subtitle: `Subtitle #0${posts.length + 1}`,
-        likes: 50
-      }
+        likes: 50,
+      },
     ]);
-  };
-
-  function handleRemove( postId){
-    setPosts((prevState) => prevState.filter(post => post.id !== postId));
   }
 
-  
+  function handleRemove(postId) {
+    setPosts((prevState) => prevState.filter((post) => post.id !== postId));
+  }
+
   return (
-    <>
-      <Header >
+    <ThemeProvider>
+      <Header>
         <h2>Posts da Semana</h2>
-        <button onClick={handleRefresh}>Atualizar</button>
       </Header>
+      <button onClick={handleRefresh}>Atualizar</button>
       <hr />
 
-      {posts.map(
-        (element) => (
-          <Post
-            key={element.title} 
-            onRemove={handleRemove}
-            post={{
-              id: element.id,
-              title : element.title,
-              subtitle: element.subtitle,
-              read: element.read
-            }}
-            likes={element.likes} 
-          />
-        )
-      )}
-    </>
+      {posts.map((element) => (
+        <Post
+          key={element.title}
+          onRemove={handleRemove}
+          post={{
+            id: element.id,
+            title: element.title,
+            subtitle: element.subtitle,
+            read: element.read,
+          }}
+          likes={element.likes}
+        />
+      ))}
+    </ThemeProvider>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useContext} from "react";
 import PropTypes from 'prop-types';
+import { ThemeContext } from "./ThemeContext";
+import Button from "./Button";
 
 export default function Header({title, children}) {
+
+  const {onToggleTheme} = useContext(ThemeContext);
+
     return (
         <>
             <h1>{title}</h1>    
+            <Button onClick={onToggleTheme}>Mudar Tema</Button>
             {children}    
         </>
     );
