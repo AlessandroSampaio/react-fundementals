62e3a5fd60dbc48f1dfe8f76bf698cab06b50f35
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-10-27 09:28:34 -0300

    Introdução ao StyledComponents

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App/";
 
+import GlobalStyle from './styles/global';
+
 ReactDom.render(
-    <App />,
+    < >
+    <GlobalStyle />
+    <App />
+    </>,
     document.getElementById('root'),
 );
