f1d048fde996e863aa87b90579a73d3a17775ea3
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-10-27 09:42:41 -0300

    Criando StyledComponents

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,22 @@ import React, {useContext} from "react";
 import PropTypes from 'prop-types';
 import { ThemeContext } from "../../context/ThemeContext";
 import Button from "../Button";
+import styled from "styled-components";
+
+
+
+const Title = styled.h1`
+    color: #637BF3;
+`;
+
+
 export default function Header({title, children}) {
 
   const {onToggleTheme} = useContext(ThemeContext);
 
     return (
         <div>
-            <h1>{title}</h1>    
+            <Title>{title}</Title>    
             <Button onClick={onToggleTheme}>Mudar Tema</Button>
             {children}    
         </div>
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import PostHeader from "./PostHeader";
 
+import { Subtitle, Rate } from "./styles";
+
 export default function Post(props) {
   return (
     <>
-      <article> 
-        <PostHeader 
+      <article>
+        <PostHeader
           onRemove={props.onRemove}
           post={{
             id: props.post.id,
@@ -14,10 +16,8 @@ export default function Post(props) {
             read: props.post.read,
           }}
         />
-        <br/>
-        <small> {props.post.subtitle} </small>
-        <br />
-        Media: {props.likes / 2}
+        <Subtitle> {props.post.subtitle} </Subtitle>
+        <Rate>Media: {props.likes / 2}</Rate>
       </article>
       <br />
     </>
