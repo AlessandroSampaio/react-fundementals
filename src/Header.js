327551b2b440a1faabd7831ea0f86a0e0a9747df
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-10-27 08:22:00 -0300

    Trabalhando com CSS Modules

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,14 @@ import React, {useContext} from "react";
 import PropTypes from 'prop-types';
 import { ThemeContext } from "./ThemeContext";
 import Button from "./Button";
-
+import styles from './header.css';
 export default function Header({title, children}) {
 
   const {onToggleTheme} = useContext(ThemeContext);
 
     return (
         <div>
-            <h1>{title}</h1>    
+            <h1 className={styles.title}>{title}</h1>    
             <Button onClick={onToggleTheme}>Mudar Tema</Button>
             {children}    
         </div>
