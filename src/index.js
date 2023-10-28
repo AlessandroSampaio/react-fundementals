7de06a834570c3aeb5341a54807a74493c8c0972
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-10-28 08:45:23 -0300

    Update to React 18.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDom from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App/";
 
 import GlobalStyle from './styles/global';
 
-ReactDom.render(
+let root = createRoot(
+    document.getElementById('root'),);
+
+root.render(
     < >
     <GlobalStyle />
     <App />
     </>,
-    document.getElementById('root'),
 );
